64bd2e9ea8c255f9a4acf9bb3fd93cb1a7796462
Sidhant <84363346+Trazer09@users.noreply.github.com>
2024-02-17 17:01:28 +0530

    completed exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,15 @@
-const reverseString = function() {
+const reverseString = function(word) {
+let letters = word.split("");
+let wordLength = letters.length;
+let reverseWord = "";
+
+for(let i = wordLength - 1; i>=0;i--)
+{
+reverseWord += letters[i];
+}
+
+return reverseWord;
+
 
 };
 
